653ffb2805a992d443c5237b53225354e6d22fdd
tanselbay1 <tanselbay1@hotmail.com>
2025-11-26 16:17:48 +0700

    Add required member functions and attributes

--- cpp05/ex00/Bureaucrat.hpp
+++ cpp05/ex00/Bureaucrat.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tanselbayraktaroglu <tanselbayraktarogl    +#+  +:+       +#+        */
+/*   By: tanselbay1 <tanselbay1@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/25 13:04:05 by tanselbayra       #+#    #+#             */
-/*   Updated: 2025/11/25 17:56:26 by tanselbayra      ###   ########.fr       */
+/*   Updated: 2025/11/26 16:08:38 by tanselbay1       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,18 +19,33 @@
 class Bureaucrat {
 	
 private:
-	std::string _name;
-	int			_grade;
+	const std::string	_name;
+	int					_grade;
 	
 public:
 	Bureaucrat(void);
 	Bureaucrat(std::string name, int grade);
-	Bureaucrat(Bureaucrat &src);
-	Bureaucrat &operator=(Bureaucrat &src);
+	Bureaucrat(const Bureaucrat &src);
+	Bureaucrat &operator=(const Bureaucrat &src);
 	~Bureaucrat(void);
 
-	std::string getName();
-	int			getGrade();
+	std::string getName() const;
+	int			getGrade() const;
+
+	void incrementGrade();
+	void decrementGrade();
+
+	class GradeTooHighException : public std::exception {
+	public:
+		virtual const char* what() const throw();
+	};
+
+	class GradeTooLowException : public std::exception {
+	public:
+		virtual const char* what() const throw();
+	};
 };
+
+std::ostream &operator<<(std::ostream &o, const Bureaucrat &rhs);
 
 #endif
